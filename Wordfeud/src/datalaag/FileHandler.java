0c81d413856c1d50391696ff0761749a43745405
TJvL <TJ@145.102.94.243>
2014-04-17 12:56:24 +0200

    > FileHandler class aangevuld met een readImage methode. (Zie interne comments voor uitleg functionaliteit)

--- Wordfeud/src/datalaag/FileHandler.java
+++ Wordfeud/src/datalaag/FileHandler.java
@@ -1,5 +1,11 @@
 package datalaag;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class FileHandler {
 
 	private static FileHandler fileHandler = new FileHandler();
@@ -9,6 +15,29 @@ public class FileHandler {
 	public static FileHandler getInstance(){
 		return fileHandler;
 	}
+	
+	/*
+	 * This method is used to buffer images for use in the application GUI.
+	 * param = Path to the file(must be an image) in question.
+	 * return = Returns the targeted image file as a BufferedImage object or returns null if an error occured.
+	 */
+	public static BufferedImage readImage(String aFileName)
+		{
+			BufferedImage image = null;
+			try
+				{
+					image = ImageIO.read(new File(aFileName));
+					System.out.println("SUCCES: Image buffered. Path "
+							+ aFileName);
+					return image;
+				}
+			catch (IOException error)
+				{
+					System.err.println(error.getMessage());
+					System.err.println(error.getStackTrace());
+					return null;
+				}
+		}
 	
 }
 
