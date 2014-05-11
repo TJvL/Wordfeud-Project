0ddcf614bc4e71262620b2d97d9c7af4b43d81b6
Bram <Bram@Bram-PC>
2014-05-11 21:04:55 +0200

    Ranking scherm gemaakt

---
+++ Wordfeud/src/gui/Ranking.java
@@ -0,0 +1,56 @@
+package gui;
+
+import java.awt.Dimension;
+import java.net.URL;
+
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
+@SuppressWarnings("serial")
+public class Ranking extends JFrame
+{
+
+	private DefaultListModel<String> myLM = new DefaultListModel<String>();
+	private JList<String> myList = new JList<String>(myLM);
+	JScrollPane mySP = new JScrollPane(myList);
+	JLabel title = new JLabel();
+;
+
+	public void showRanking()
+	{
+
+		this.setLayout(null);
+		this.setPreferredSize(new Dimension(400, 550));
+
+		for (int i = 0; i < 24; i++)
+		{
+			if(i < 9)
+			{
+				myLM.add(i, i+1 + ":   Hoi");
+			}
+			else
+			{
+				myLM.add(i, i+1 + ": Hoi");
+			}
+			
+		}
+		this.add(mySP);
+		this.add(title);
+		title.setText("Ranking of competition : Comp Name.");
+		title.setBounds(75, 10, 225, 25);
+		mySP.setBounds(100, 50,200,435);
+		
+		
+
+		this.pack();
+		this.setVisible(true);
+		this.setResizable(false);
+	}
+
+
+}
