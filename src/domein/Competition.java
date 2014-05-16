0178da14d6bd7b59bf00cab8117204133e69ad40
Thomas <Thomas@145.102.94.109>
2014-05-16 10:17:26 +0200

    test

--- src/domein/Competition.java
+++ src/domein/Competition.java
@@ -3,26 +3,33 @@ package domein;
 public class Competition {
 	
 	private int databaseID;
+	private String compOwner;
 	private String startDate;
 	private String endDate;
 	private String summary;
-	private int maxParticipants;
 	private int minParticipants;
-	private String compOwner;
+	private int maxParticipants;
+	private int amountParticipants;
 	
-	public Competition(int databaseID, String startDate, String endDate, String summary, int maxParticipants, int minParticipants){
+	public Competition(int databaseID, String compOwner, String startDate, String endDate, String summary, int minParticipants, int maxParticipants){
 		this.databaseID = databaseID;
+		this.compOwner = compOwner;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.summary = summary;
-		this.maxParticipants = maxParticipants;
 		this.minParticipants = minParticipants;
+		this.maxParticipants = maxParticipants;
+		
 	}
 
 	public int getDatabaseID() {
 		return databaseID;
 	}
 
+	public String getCompOwner() {
+		return compOwner;
+	}
+	
 	public String getStartDate() {
 		return startDate;
 	}
@@ -34,17 +41,21 @@ public class Competition {
 	public String getSummary() {
 		return summary;
 	}
+	
+	public int getMinParticipants() {
+		return minParticipants;
+	}
 
 	public int getMaxParticipants() {
 		return maxParticipants;
 	}
 
-	public int getMinParticipants() {
-		return minParticipants;
-	}
+	public int getAmountParticipants(){
+			return amountParticipants;
+		}
 
-	public String getCompOwner() {
-		return compOwner;
-	}
+	public void setAmountParticipants(int amountParticipants){
+			this.amountParticipants = amountParticipants;
+		}
 	
 }
---
+++ src/domein/CompetitionManager.java
@@ -0,0 +1,49 @@
+package domein;
+
+import java.util.ArrayList;
+
+import datalaag.DatabaseHandler;
+
+public class CompetitionManager {
+	private WordFeud wf;
+	private ArrayList<Competition> joinedCompetitions;
+
+	public CompetitionManager(WordFeud wf) {
+		this.wf = wf;
+		joinedCompetitions = new ArrayList<Competition>();
+	}
+
+	public void loadCompetitions(String username) {
+		
+		DatabaseHandler dbh = DatabaseHandler.getInstance();
+		joinedCompetitions.clear();
+		System.out.println("Loading competitions...");
+
+		ArrayList<String> comps = dbh.fetchCompetition(username);
+		if (!comps.isEmpty()) {
+			for (String comp : comps) {
+				String[] compData = comp.split("---");
+
+				// --------TEST CODE---------
+				System.out.println("RAW DATA PRINT COMP: " + compData[0] + " "
+						+ compData[1] + " " + compData[2] + " " + compData[3]
+						+ " " + compData[4] + " " + compData[5] + " " + compData[6]);
+				// --------TEST CODE---------
+
+				joinedCompetitions.add(new Competition(Integer
+						.parseInt(compData[0]), compData[1], compData[2], compData[3],
+						compData[4], Integer.parseInt(compData[5]), Integer
+								.parseInt(compData[6])));
+			}
+			System.out.println("Succesfully loaded competitions.");
+		} else {
+			System.out.println("No competitions to load.");
+		}
+	}
+	
+	public void createCompetition(String currentUsername, String summary, String endDate, int maxParticipants) {
+		
+		DatabaseHandler dbh = DatabaseHandler.getInstance();
+		dbh.createCompetition(currentUsername, summary, endDate, maxParticipants);
+	}
+}
